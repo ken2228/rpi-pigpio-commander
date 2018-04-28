2550af3eff61ee49f3098110dfd2b0f09530fcb4
ken2228 <danny_sleugel@hotmail.com>
2018-04-28 21:35:32 +0200

    jpigpio template added

--- src/rpiPigpioCommander.java
+++ src/rpiPigpioCommander.java
@@ -1,9 +1,31 @@
+import jpigpio.JPigpio;
+import jpigpio.PigpioException;
+import jpigpio.PigpioSocket;
+import jpigpio.Utils;
 
 public class rpiPigpioCommander {
-
+	
+	final String raspberryIP = "192.168.178.122";
+	final int raspberryPort = 8888;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Test_AppName");
+		rpiPigpioCommander app = new rpiPigpioCommander();
+		app.run();
+	}
+	
+	public void run() {
 
+		try {
+			JPigpio pigpio = new PigpioSocket(raspberryIP, raspberryPort);
+			//JPigpio pigpio = new Pigpio();
+			pigpio.gpioInitialize();
+			Utils.addShutdown(pigpio);
+			
+		} catch (PigpioException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
